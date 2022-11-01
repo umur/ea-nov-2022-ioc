8adf051babd2ffe5a3e13ceb43e3bf834048eb07
Nehemiah Tefera <nehemiaha1@outlook.com>
2022-11-01 02:01:18 -0500

    Initial commit

---
+++ IoC/Main.java
@@ -0,0 +1,9 @@
+package IoC;
+
+public class Main {
+    @MyAutowired
+    private String n;
+    public static void main(String[] args) {
+        System.out.println("nehi");
+    }
+}
---
+++ IoC/MyInjector.java
@@ -0,0 +1,16 @@
+package IoC;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyInjector {
+    private Map<Class, Object> beans = new HashMap<>();
+
+    private void scanAnnotations() {
+
+    }
+
+    public Object getBean(Class clazz) {
+        return null;
+    }
+}
