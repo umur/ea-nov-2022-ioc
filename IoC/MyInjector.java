c778c047622e34d4c6be51925971ab09746ffe6b
Nehemiah Tefera <nehemiaha1@outlook.com>
2022-11-01 02:54:31 -0500

    Grab annotated classes in HashMap

--- IoC/MyInjector.java
+++ IoC/MyInjector.java
@@ -1,13 +1,30 @@
 package IoC;
 
+import classFinder.ClassFinder;
+
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MyInjector {
     private Map<Class, Object> beans = new HashMap<>();
 
     private void scanAnnotations() {
+        List<Class<?>> classes = ClassFinder.find("IoC");
 
+        for (Class<?> cl : classes) {
+            if (cl.isAnnotationPresent(MyBean.class)
+                    || cl.isAnnotationPresent(MyAutowired.class)) {
+                System.out.println(cl.getSimpleName());
+                try {
+                    beans.put(cl, cl.getConstructor().newInstance());
+                } catch (NoSuchMethodException ignore) {
+                } catch (InstantiationException ignore){
+                } catch (IllegalAccessException ignore) {
+                } catch (InvocationTargetException ignore){}
+            }
+        }
     }
 
     public Object getBean(Class clazz) {
