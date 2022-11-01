c7b1a07e116224e26d92ea823c7accc0a9fdd71f
crisnaxtha <krishnashrestha49@gmail.com>
2022-11-01 18:45:38 -0500

    Inital Commit

---
+++ src/main/java/com/example/eanov2022ioc/EaNov2022IocApplication.java
@@ -0,0 +1,13 @@
+package com.example.eanov2022ioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaNov2022IocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaNov2022IocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eanov2022ioc/EaNov2022IocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eanov2022ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaNov2022IocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
