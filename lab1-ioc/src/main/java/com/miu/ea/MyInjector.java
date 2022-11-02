34da2465386d13c6dfeed7d33ce1a9abff3b2476
sukherde <sukh.erde@gmail.com>
2022-11-01 22:38:07 -0500

    Added field injection by searching MyAutowired annotation

--- lab1-ioc/src/main/java/com/miu/ea/MyInjector.java
+++ lab1-ioc/src/main/java/com/miu/ea/MyInjector.java
@@ -4,18 +4,19 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("rawtypes")
 public class MyInjector {
-    private HashMap<Class, Object> mapForObjects = new HashMap<>();
+    private final HashMap<Class<?>, Object> mapForObjects = new HashMap<>();
     private final String[] myAnnotations = {"MyBean"};
     public void createInstances(String packageName) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         List<Class> listOfClasses = findAllClassesUsingClassLoader(packageName);
 
-        for (Class curClass : listOfClasses) {
+        for (Class<?> curClass : listOfClasses) {
 //            System.out.println(curClass);
             for (Annotation annotation : Arrays.stream(curClass.getAnnotations()).toList()) {
                 String[] splitAnnotation = annotation.toString().split("\\.");
@@ -27,9 +28,25 @@ public class MyInjector {
                 if (Arrays.asList(myAnnotations).contains(annotationName)){
                     //TODO add to mapping if found annotation
                     if (!mapForObjects.containsKey(curClass)){
-                        Object newObject = curClass.getConstructor().newInstance();
-                        mapForObjects.put(curClass, newObject);
+                        Object newObject = curClass.getDeclaredConstructor().newInstance();
                         System.out.printf("Object %s is created for the class %s %n", newObject, curClass);
+
+                        for(Field field : Arrays.stream(curClass.getDeclaredFields()).filter(f -> f.isAnnotationPresent(MyAutowired.class)).toList()) {
+                            Class<?> classToCreateAnObject = field.getType();
+                            Object fieldObject;
+
+                            if (!mapForObjects.containsKey(classToCreateAnObject)){
+                                fieldObject = classToCreateAnObject.getDeclaredConstructor().newInstance();
+                                mapForObjects.put(classToCreateAnObject, fieldObject);
+                            } else {
+                                fieldObject = mapForObjects.get(classToCreateAnObject);
+                            }
+
+                            field.setAccessible(true);
+                            field.set(newObject, fieldObject);
+
+                        }
+                        mapForObjects.put(curClass, newObject);
                     }
                 }
             }
@@ -41,7 +58,7 @@ public class MyInjector {
                 .getResourceAsStream(packageName.replaceAll("[.]", "/"));
         BufferedReader reader;
         if (stream == null) {
-            return null;
+            return new ArrayList<>();
         }
         reader = new BufferedReader(new InputStreamReader(stream));
         return reader.lines()
