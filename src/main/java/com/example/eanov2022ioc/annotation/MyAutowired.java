2d1082415b529f996404a9fe7801debf1b99ec4c
crisnaxtha <krishnashrestha49@gmail.com>
2022-11-01 19:06:53 -0500

    Added Annotation

---
+++ src/main/java/com/example/eanov2022ioc/annotation/MyAutowired.java
@@ -0,0 +1,11 @@
+package com.example.eanov2022ioc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface MyAutowired {
+}
